3e688e1fadddf5d8e4ddad343ee99beb410e3309
Dafu Lv <dafu.lv@gmail.com>
2024-09-29 18:38:46 +0800

    Update rmm.sh to remove $/plat/host.
    Signed-off-by: Dafu Lv <dafu.lv@gmail.com>

--- rmm.sh
+++ rmm.sh
@@ -16,12 +16,14 @@ fi
 
 pushd $SRC_DIR >& /dev/null
 
-find . -name "*.[chS]" > ./debug/.all.tmp
-sed -e '/fake_host/d' ./debug/.all.tmp > ./debug/.part.000.tmp
-sed -e '/\/tests\//d' ./debug/.part.000.tmp > ./debug/.part.001.tmp
-sed -e '/\/test\//d' ./debug/.part.001.tmp > ./debug/.part.002.tmp
-sed -e '/\/debug\//d' ./debug/.part.002.tmp > ./debug/.part.003.tmp
-cat ./debug/.part.003.tmp | sort > ./debug/src-to-idx
+find . -name "*.[chS]"                              > ./debug/.all.tmp
+sed -e '/fake_host/d'       ./debug/.all.tmp        > ./debug/.part.000.tmp
+sed -e '/\/tests\//d'       ./debug/.part.000.tmp   > ./debug/.part.001.tmp
+sed -e '/\/test\//d'        ./debug/.part.001.tmp   > ./debug/.part.002.tmp
+sed -e '/\/debug\//d'       ./debug/.part.002.tmp   > ./debug/.part.003.tmp
+sed -e '/\/plat\/host\//d'  ./debug/.part.003.tmp   > ./debug/.part.004.tmp
+
+cat ./debug/.part.004.tmp | sort > ./debug/src-to-idx
 
 cscope -Rkb -i ./debug/src-to-idx &
 ctags -R -L ./debug/src-to-idx &
